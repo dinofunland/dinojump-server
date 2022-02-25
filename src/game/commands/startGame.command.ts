bcfb8e8978ae3dd65a0d761068d98f78c878c34a
Wenish <Wenish@users.noreply.github.com>
2022-02-25 02:44:45 +0100

    feat: finish game process loop

--- src/game/commands/startGame.command.ts
+++ src/game/commands/startGame.command.ts
@@ -1,25 +1,25 @@
 import { Command } from "@colyseus/command";
 import { Room } from "colyseus";
 import logger from "../../services/logger.services";
+import { GameRoom } from "../game.room";
 import { GameStep, GameState } from "../game.state";
 import { EndGameCommand } from "./endGame.command";
 
 interface StartGamePayload { }
 
-export class StartGameCommand extends Command<Room<GameState>, StartGamePayload> {
+export class StartGameCommand extends Command<GameRoom, StartGamePayload> {
     execute(payload: StartGamePayload) {
         logger('Start Game', 'Command')
-        if (this.state.gameStep != GameStep.LOBBY) throw 'game isnt in lobby step';
+        if (this.state.gameStep != GameStep.LOBBY) return;
         this.room.lock()
         this.state.gameStep = GameStep.STARTING
 
         this.clock.setTimeout(() => {
             this.state.gameStep = GameStep.ONGOING
+
+            this.clock.setTimeout(() => {
+                this.room.dispatcher.dispatch(new EndGameCommand())
+            }, 10_000)
         }, 5_000)
-        return
-        this.clock.setTimeout(() => {
-            // Do this command in 60 seconds
-            new EndGameCommand()
-        }, 10_000)
     }
 }
