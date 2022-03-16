97b2a0051eb038a8f4f67eb11b5a7d3d16ad0c6b
Wenish <Wenish@users.noreply.github.com>
2022-03-16 13:56:39 +0100

    Update startGame.command.ts

--- src/game/commands/startGame.command.ts
+++ src/game/commands/startGame.command.ts
@@ -4,6 +4,7 @@ import logger from '../../services/logger.services'
 import { GameRoom } from '../game.room'
 import { GameStep } from '../game.state'
 import { EndGameCommand } from './endGame.command'
+import { RemoveAllPlatformsCommand } from './removeAllPlatforms.command'
 import { ResetPlayersCommand } from './resetPlayers.command'
 
 interface StartGamePayload {}
@@ -15,6 +16,7 @@ export class StartGameCommand extends Command<GameRoom, StartGamePayload> {
     this.room.lock()
     this.state.gameStep = GameStep.STARTING
     this.room.dispatcher.dispatch(new ResetPlayersCommand())
+    this.room.dispatcher.dispatch(new RemoveAllPlatformsCommand())
 
     this.clock.setTimeout(() => {
       this.state.gameStep = GameStep.ONGOING
